5f5ef7a674a7b925765be352aafab6ca4298c113
rickdev47-ai <rickdev47@gmail.com>
2026-02-04 14:40:24 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 const mysql = require("mysql2");
 
-<<<<<<< HEAD
 const db = mysql.createPool(process.env.DATABASE_URL);
 
 db.getConnection((err) => {
@@ -8,17 +7,9 @@ db.getConnection((err) => {
         console.error("❌ MySQL connection failed:", err);
     } else {
         console.log("✅ MySQL connected (ONLINE)");
-=======
-const db = mysql.createConnection(process.env.DATABASE_URL);
-
-db.connect(err => {
-    if (err) {
-        console.error("❌ MySQL connection failed:", err);
-    } else {
-        console.log("✅ MySQL connected (Railway)");
->>>>>>> 86c99fa6ebe492fc637fd3e1d4dc914af042c85b
     }
 });
 
 module.exports = db;
+
 
